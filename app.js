3d4bfd73342c4c4a03a36417c5a322b58f2dcdd5
xdcao@zju.edu.cn <EAST896cao>
2021-02-03 12:44:54 -0700

    edit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const Campground = require('./models/compground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -21,6 +22,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -47,19 +49,18 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground })
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/edit', { campground })
+})
 
-
-
-
-
-
-
-
-// app.get('/makecampground', async (req, res) => {
-//     const camp = new Campground({ title: 'My Backyard', description: 'cheap camping' })
-//     await camp.save()
-//     res.send(camp)
-// })
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    //Why not res.redirect(`campgrounds/${campground._id}`) Here
+    res.redirect(`${campground._id}`)
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
