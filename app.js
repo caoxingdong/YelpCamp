e4a55c75e3bbdf475220d5571ed9aae4e0cdb7be
xdcao@zju.edu.cn <EAST896cao>
2021-02-03 01:38:21 -0700

    show & new

--- app.js
+++ app.js
@@ -28,6 +28,24 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/show', { campground })
+})
+
+
+
+
+
+
+
+
+
 // app.get('/makecampground', async (req, res) => {
 //     const camp = new Campground({ title: 'My Backyard', description: 'cheap camping' })
 //     await camp.save()
