ff99a9ac7973a63ebb6347b60a9ffd76c30f16f3
xdcao@zju.edu.cn <EAST896cao>
2021-02-12 21:51:32 -0700

    authtication

--- routes/users.js
+++ routes/users.js
@@ -12,13 +12,16 @@ router.get('/login', (req, res) => {
     res.render('users/login')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body
         const user = new User({ email, username })
         const registeredUser = await User.register(user, password)
-        req.flash('success', 'Welcome to Yelp Camp')
-        res.redirect('/campgrounds')
+        req.login(registeredUser, err => {
+            if (err) return next(err)
+            req.flash('success', 'Welcome to Yelp Camp')
+            res.redirect('/campgrounds')
+        })
     } catch (e) {
         req.flash('error', e.message)
         res.redirect('register')
@@ -28,6 +31,14 @@ router.post('/register', catchAsync(async (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
     req.flash('success', 'welcome back')
+    const redirectUrl = req.session.returnTo || '/campgrounds'
+    delete req.session.returnTo
+    res.redirect(redirectUrl)
+})
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    req.flash('success', 'Goodbye')
     res.redirect('/campgrounds')
 })
 
