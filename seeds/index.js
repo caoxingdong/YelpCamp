0f060acbad064e730e43dc55b7a82203cbec7245
xdcao@zju.edu.cn <EAST896cao>
2021-02-14 17:12:47 -0700

    cluster map

--- seeds/index.js
+++ seeds/index.js
@@ -18,7 +18,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedBD = async () => {
     await Campground.deleteMany({})
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
@@ -27,7 +27,10 @@ const seedBD = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             geometry: {
                 type: "Point",
-                coordinates: [-113.1331, 47.0202]
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
             },
             images: [
                 {
